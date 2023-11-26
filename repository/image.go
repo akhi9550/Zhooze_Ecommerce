97f4f92af4ab599c2bf56aee30c59a4074ae6afc
akhi9550 <akhilc248@gmail.com>
2023-11-26 14:30:06 +0530

    small updates

---
+++ repository/image.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"Zhooze/db"
+	"Zhooze/utils/models"
+)
+
+func ShowImages(productID int) ([]models.Image, error) {
+	var image []models.Image
+	err := db.DB.Raw(`SELECT url FROM images  WHERE images.product_id = $1`, productID).Scan(&image).Error
+	if err != nil {
+		return nil, err
+	}
+	return image, nil
+}
---
+++ usecase/image.go
@@ -0,0 +1,14 @@
+package usecase
+
+import (
+	"Zhooze/repository"
+	"Zhooze/utils/models"
+)
+
+func ShowImages(productID int) ([]models.Image, error) {
+	image, err := repository.ShowImages(productID)
+	if err != nil {
+		return nil, err
+	}
+	return image, nil
+}
